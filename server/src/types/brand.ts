1d74b59c3500288bc2f9b38c387d82baa80d4fb9
JoshTeflon <jenikele21h@gmail.com>
2026-02-26 09:44:36 +0100

    brand-workflow/feat: added Brand details interface to brand types

--- server/src/types/brand.ts
+++ server/src/types/brand.ts
@@ -1,3 +1,10 @@
+export interface IBrand {
+  id: string;
+  name: string;
+  description?: string;
+  logo_url?: string;
+};
+
 export type CreateBrandInput = {
   name: string;
   description?: string;
