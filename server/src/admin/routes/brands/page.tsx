0e3730bf1cb1c5bdd93f25d33deed8e190afeaed
JoshTeflon <jenikele21h@gmail.com>
2026-03-03 10:23:38 +0100

    brand-workflow/feat: added functionality to retrieve brand list with pagination setup

--- server/src/admin/routes/brands/page.tsx
+++ server/src/admin/routes/brands/page.tsx
@@ -1,9 +1,66 @@
 import { defineRouteConfig } from "@medusajs/admin-sdk";
-import { Container } from "@medusajs/ui";
+import {
+  Container,
+  createDataTableColumnHelper,
+  DataTable,
+  DataTablePaginationState,
+  Heading,
+  useDataTable,
+} from "@medusajs/ui";
 import { TagSolid } from "@medusajs/icons";
 
+import { useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query"
+
+import { sdk } from "../../lib/sdk";
+
+import { BrandsResponse, IBrand } from "../../../types/brand";
+
 const BrandsPage = () => {
-  // TODO: Fetch and display brands list
+  const columnHelper = createDataTableColumnHelper<IBrand>();
+  
+  const columns = [
+    columnHelper.accessor("id", {
+      header: "ID",
+    }),
+    columnHelper.accessor("name", {
+      header: "Name",
+    }),
+    columnHelper.accessor("description", {
+      header: "Description",
+    }),
+  ];
+
+  const limit = 15 as const;
+
+  const [pagination, setPagination] = useState<DataTablePaginationState>({
+    pageIndex: 0,
+    pageSize: limit,
+  });
+  
+  const offset = useMemo(() => pagination.pageIndex * limit, [pagination]);
+
+  const { data, isLoading } = useQuery<BrandsResponse>({
+    queryFn: () => sdk.client.fetch('/admin/brands', {
+      query: {
+        limit,
+        offset,
+      },
+    }),
+    queryKey: [["brands", limit, offset]],
+  });
+
+  const table = useDataTable({
+    columns,
+    data: data?.brands ?? [],
+    getRowId: (row) => row.id,
+    rowCount: data?.count ?? 0,
+    isLoading,
+    pagination: {
+      state: pagination,
+      onPaginationChange: setPagination,
+    }
+  });
 
   return (
     <Container className="divide-y p-0">
